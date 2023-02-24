77c106bbd7f2e0eba1eb656fd6eedc758acdc691
Josh Hoover <caboodlennm@gmail.com>
2023-02-24 12:37:05 -0600

    Make sliders all on svg

--- src/index.ts
+++ src/index.ts
@@ -106,13 +106,13 @@ const drawRoundingMask = (svg: SVGElement, x: number, y: number, theta: number) 
 
   const largeArcFlag = endAngle - startAngle > Math.PI ? 1 : 0;
 
-  const theta1 = theta + (Math.PI * 1) / 3;
-  const bezierControl1X = endX + 80 * Math.cos(theta1);
-  const bezierControl1Y = endY + 80 * Math.sin(theta1);
+  const theta1 = theta + Math.PI / 3;
+  const bezierControl1X = endX + 90 * Math.cos(theta1);
+  const bezierControl1Y = endY + 90 * Math.sin(theta1);
 
-  const theta2 = theta - (Math.PI * 1) / 3;
-  const bezierControl2X = startX + 80 * Math.cos(theta2);
-  const bezierControl2Y = startY + 80 * Math.sin(theta2);
+  const theta2 = theta - Math.PI / 3;
+  const bezierControl2X = startX + 90 * Math.cos(theta2);
+  const bezierControl2Y = startY + 90 * Math.sin(theta2);
 
   let pathStr = `M ${startX} ${startY}`;
   pathStr += ` A ${innerRadius} ${innerRadius} 0 ${largeArcFlag} 1 ${endX} ${endY}`;
@@ -145,8 +145,31 @@ const maskSides = (svg: SVGElement, boxHeight: number, boxWidth: number, sideMas
   svg.appendChild(rightRect);
 };
 
-const mirrorAngleVertically = (angle: number) => {
-  return Math.PI - angle;
+const drawSimpleTrack = (svg: SVGElement, boxHeight: number, boxWidth: number, xPadding: number) => {
+  const thickness = 10;
+
+  // draw a path with rounded corners
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+  const topOffset = boxHeight / 2 - thickness / 2;
+  const botOffset = boxHeight / 2 + thickness / 2;
+
+  const pathStartX = xPadding;
+  const pathEndX = boxWidth - xPadding;
+
+  let pathStr = `M ${pathStartX} ${topOffset}`;
+  pathStr += ` L ${pathEndX} ${topOffset}`;
+  pathStr += ` A ${thickness / 2} ${thickness / 2} 0 0 1 ${pathEndX} ${botOffset}`;
+  pathStr += ` L ${pathStartX} ${botOffset}`;
+  pathStr += ` A ${thickness / 2} ${thickness / 2} 0 0 1 ${pathStartX} ${topOffset}`;
+  pathStr += ` Z`;
+
+  path.setAttribute("d", pathStr);
+  path.setAttribute("stroke-width", "0");
+  path.setAttribute("stroke", "black");
+  path.setAttribute("fill", "black");
+
+  svg.appendChild(path);
 };
 
 const toRun = () => {
@@ -167,22 +190,131 @@ const toRun = () => {
   const boxHeight = 100;
   const boxWidth = 500;
 
+  const footerHeight = 50;
+
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
   svg.setAttribute("width", boxWidth.toString());
-  svg.setAttribute("height", boxHeight.toString());
+  svg.setAttribute("height", (boxHeight + footerHeight).toString());
   svg.setAttribute("preserveAspectRatio", "none");
+
+  const mainGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+  mainGroup.setAttribute("width", boxWidth.toString());
+  mainGroup.setAttribute("height", boxHeight.toString());
+
+  svg.appendChild(mainGroup);
 
   // State variables
   // 0 to 2pi
   let phase = 0;
+  let draggingPhase = false;
   let wavelength = 500;
+  let draggingWavelength = false;
   // Thumb coordinate is between 0 and 1
   let thumbCoordinate = 0.5;
   let thumbInertia = 0;
   let dragging = false;
 
+  const minWavelength = 100;
+  const maxWavelength = 2000;
+
+  const simpleSliderXPadding = 20;
+
+  // Phase slider
+  const phaseGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+  phaseGroup.setAttribute("width", (boxWidth / 2).toString());
+  phaseGroup.setAttribute("height", footerHeight.toString());
+  phaseGroup.setAttribute("transform", `translate(0, ${boxHeight})`);
+
+  svg.appendChild(phaseGroup);
+
+  drawSimpleTrack(phaseGroup, footerHeight, boxWidth / 2, simpleSliderXPadding);
+
+  const drawPhaseThumb = () => {
+    const thumbXCoordinate = (phase / (2 * Math.PI)) * (boxWidth / 2 - 2 * simpleSliderXPadding) + simpleSliderXPadding;
+
+    const thumb = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    thumb.setAttribute("cx", thumbXCoordinate.toString());
+    thumb.setAttribute("cy", (footerHeight / 2).toString());
+    thumb.setAttribute("r", "10");
+    thumb.setAttribute("fill", "blue");
+    thumb.setAttribute("stroke", "blue");
+    thumb.setAttribute("stroke-width", "0");
+
+    phaseGroup.appendChild(thumb);
+
+    thumb.onmousedown = (e) => {
+      if (e.button !== 0) {
+        return;
+      }
+
+      draggingPhase = true;
+      e.stopPropagation();
+    };
+  };
+
+  const redrawPhaseThumb = () => {
+    // Remove the old thumb
+    const thumb = phaseGroup.querySelector("circle");
+    if (thumb) {
+      phaseGroup.removeChild(thumb);
+    }
+    drawPhaseThumb();
+  };
+
+  drawPhaseThumb();
+
+  // Wavelength slider
+  const wavelengthGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+  wavelengthGroup.setAttribute("width", (boxWidth / 2).toString());
+  wavelengthGroup.setAttribute("height", footerHeight.toString());
+  wavelengthGroup.setAttribute("transform", `translate(${boxWidth / 2}, ${boxHeight})`);
+
+  svg.appendChild(wavelengthGroup);
+
+  drawSimpleTrack(wavelengthGroup, footerHeight, boxWidth / 2, simpleSliderXPadding);
+
+  const drawWavelengthThumb = () => {
+    const thumbXCoordinate =
+      ((wavelength - minWavelength) / (maxWavelength - minWavelength)) * (boxWidth / 2 - 2 * simpleSliderXPadding) + simpleSliderXPadding;
+
+    const thumb = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    thumb.setAttribute("cx", thumbXCoordinate.toString());
+    thumb.setAttribute("cy", (footerHeight / 2).toString());
+    thumb.setAttribute("r", "10");
+    thumb.setAttribute("fill", "blue");
+    thumb.setAttribute("stroke", "blue");
+    thumb.setAttribute("stroke-width", "0");
+
+    wavelengthGroup.appendChild(thumb);
+
+    thumb.onmousedown = (e) => {
+      if (e.button !== 0) {
+        return;
+      }
+
+      draggingWavelength = true;
+      e.stopPropagation();
+    };
+  };
+
+  const redrawWavelengthThumb = () => {
+    // Remove the old thumb
+    const thumb = wavelengthGroup.querySelector("circle");
+    if (thumb) {
+      wavelengthGroup.removeChild(thumb);
+    }
+    drawWavelengthThumb();
+  };
+
+  drawWavelengthThumb();
+
+  // Main slider
   const amplitude = 30;
-  const leftWindowBound = 30;
+  const leftWindowBound = 20;
   const rightWindowBound = boxWidth - leftWindowBound;
 
   const derivativeAt = (x: number) => {
@@ -220,7 +352,7 @@ const toRun = () => {
 
     thumb.setAttribute("r", "10");
     thumb.setAttribute("fill", "blue");
-    svg.appendChild(thumb);
+    mainGroup.appendChild(thumb);
 
     if (!dragging) {
       thumb.onmousedown = (e) => {
@@ -237,17 +369,16 @@ const toRun = () => {
 
   const redrawThumb = () => {
     // clear thumb
-    svg.removeChild(svg.lastChild);
+    mainGroup.removeChild(mainGroup.lastChild);
     drawThumb();
   };
 
   const drawSvg = () => {
-    // clear svg
-    while (svg.firstChild) {
-      svg.removeChild(svg.firstChild);
+    while (mainGroup.firstChild) {
+      mainGroup.removeChild(mainGroup.firstChild);
     }
 
-    drawTrack(svg, phase, wavelength, boxHeight, boxWidth, amplitude);
+    drawTrack(mainGroup, phase, wavelength, boxHeight, boxWidth, amplitude);
 
     const leftBoxIntersectionY = valueAt(leftWindowBound);
     const rightBoxIntersectionY = valueAt(rightWindowBound);
@@ -264,71 +395,85 @@ const toRun = () => {
     const leftTheta = Math.atan2(leftRise, leftRun);
     const rightTheta = Math.atan2(rightRise, rightRun);
 
-    drawRoundingMask(svg, leftWindowBound, leftBoxIntersectionY, leftTheta + Math.PI);
-    drawRoundingMask(svg, rightWindowBound, rightBoxIntersectionY, Math.PI - rightTheta);
+    drawRoundingMask(mainGroup, leftWindowBound, leftBoxIntersectionY, leftTheta + Math.PI);
+    drawRoundingMask(mainGroup, rightWindowBound, rightBoxIntersectionY, Math.PI - rightTheta);
 
-    maskSides(svg, boxHeight, boxWidth, leftWindowBound - 10);
+    maskSides(mainGroup, boxHeight, boxWidth, leftWindowBound - 10);
 
     drawThumb();
   };
 
   drawSvg();
-
-  const wavelengthSlider = document.createElement("input");
-  wavelengthSlider.type = "range";
-  wavelengthSlider.min = "100";
-  wavelengthSlider.max = "1000";
-  wavelengthSlider.value = "500";
-  wavelengthSlider.oninput = () => {
-    wavelength = parseInt(wavelengthSlider.value);
-    drawSvg();
-  };
-
-  const phaseSlider = document.createElement("input");
-  phaseSlider.type = "range";
-  phaseSlider.min = "0";
-  phaseSlider.max = "100";
-  phaseSlider.value = "0";
-  phaseSlider.oninput = () => {
-    phase = (parseInt(phaseSlider.value) / 100) * 2 * Math.PI;
-    drawSvg();
-  };
-
+  
   controlContainer.appendChild(svg);
 
   svg.onmousemove = (e) => {
-    if (!dragging) {
+    const x = e.offsetX;
+
+    if (dragging) {
+      let newThumbCoordinate = (x - leftWindowBound) / (rightWindowBound - leftWindowBound);
+      if (newThumbCoordinate < 0) {
+        newThumbCoordinate = 0;
+      } else if (newThumbCoordinate > 1) {
+        newThumbCoordinate = 1;
+      }
+      if (newThumbCoordinate !== thumbCoordinate) {
+        thumbCoordinate = newThumbCoordinate;
+        redrawThumb();
+      }
+    }
+
+    if (draggingPhase) {
+      let newPhase = ((x - simpleSliderXPadding) / (boxWidth / 2 - simpleSliderXPadding * 2)) * 2 * Math.PI;
+
+      if (newPhase < 0) {
+        newPhase = 0;
+      } else if (newPhase > 2 * Math.PI) {
+        newPhase = 2 * Math.PI;
+      }
+      if (newPhase !== phase) {
+        phase = newPhase;
+        redrawPhaseThumb();
+        drawSvg();
+      }
+    }
+
+    if (draggingWavelength) {
+      let newWavelength =
+        ((x - simpleSliderXPadding - boxWidth / 2) / (boxWidth / 2 - simpleSliderXPadding * 2)) * (maxWavelength - minWavelength) +
+        minWavelength;
+
+      if (newWavelength < minWavelength) {
+        newWavelength = minWavelength;
+      } else if (newWavelength > maxWavelength) {
+        newWavelength = maxWavelength;
+      }
+      if (newWavelength !== wavelength) {
+        wavelength = newWavelength;
+        redrawWavelengthThumb();
+        drawSvg();
+      }
+    }
+  };
+
+  svg.onmouseleave = () => {
+    dragging = false;
+    draggingPhase = false;
+    draggingWavelength = false;
+  };
+
+  svg.onmouseup = (e) => {
+    if (e.button !== 0) {
       return;
     }
 
-    const x = e.offsetX;
-
-    let newThumbCoordinate = (x - leftWindowBound) / (rightWindowBound - leftWindowBound);
-    if (newThumbCoordinate < 0) {
-      newThumbCoordinate = 0;
-    } else if (newThumbCoordinate > 1) {
-      newThumbCoordinate = 1;
-    }
-    if (newThumbCoordinate !== thumbCoordinate) {
-      thumbCoordinate = newThumbCoordinate;
-      redrawThumb();
-    }
-
-    svg.onmouseleave = () => {
-      dragging = false;
-    };
-
-    svg.onmouseup = () => {
-      if (e.button !== 0) {
-        return;
-      }
-
-      dragging = false;
-    };
+    dragging = false;
+    draggingPhase = false;
+    draggingWavelength = false;
   };
 
-  app.appendChild(wavelengthSlider);
-  app.appendChild(phaseSlider);
+  // app.appendChild(wavelengthSlider);
+  // app.appendChild(phaseSlider);
 
   app.appendChild(phoneView);
 
